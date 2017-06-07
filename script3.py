191aeb52cf715eff16f7319ead30257035225ab3
Ian Hawke <I.Hawke@soton.ac.uk>
2017-06-07 11:37:04 +0100

    First pass through loops, containers etc.

---
+++ script3.py
@@ -0,0 +1,19 @@
+def add(x, y):
+    """
+    Add two numbers
+    
+    Parameters
+    ----------
+    
+    x : float
+        First input
+    y : float
+        Second input
+    
+    Returns
+    -------
+    
+    x + y : float
+    """
+    return x + y
+
